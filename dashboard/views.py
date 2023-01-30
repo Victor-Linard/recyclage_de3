e74eac1ff1c08f61803438e2caac8f62041b1a88
victorlinard <contact@victor-linard.fr>
2023-01-30 11:18:00 +0100

    detect type of waste

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,17 @@ from django.views.decorators.http import require_GET, require_http_methods
 import pillow_heif
 from PIL import Image
 from pillow_heif import register_heif_opener
+from math import floor
+import json
+from torchvision import datasets, transforms, models
+import torch
+import torch.nn as nn
+import json
+import os
+import certifi
+
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+os.environ["SSL_CERT_FILE"] = certifi.where()
 
 
 # Create your views here.
@@ -24,6 +35,7 @@ def dashboard(request):
             if len(request.FILES.getlist('file')) > 1:
                 resize_picture(anonyme_id)
                 check_for_duplicate(anonyme_id)
+            detect_type_of_waste(anonyme_id)
     return render(request, 'dashboard.html', context={'form': forms.UploadPictureToAnalyze})
 
 
@@ -100,3 +112,118 @@ def handle_uploaded_file(files, anonyme_id):
         with open(save_dir, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
+
+
+def detect_type_of_waste(anonyme_id):
+    model = Model('./dashboard/cnn2.pth', 'cpu')
+    CLASS_MAPPING = ['metal', 'plastic', 'cardboard', 'paper', 'trash', 'glass']
+    COLORS = np.random.uniform(0, 255, size=(len(CLASS_MAPPING), 3))
+
+    for img in os.listdir('UPLOADED_IMAGES/' + anonyme_id):
+        inference, confidence = model.infer('UPLOADED_IMAGES/' + anonyme_id+'/'+img)
+        class_name = render_prediction(inference)
+        confidence = floor(confidence * 10000) / 100
+
+        print(f'{img} - {class_name[0]} : {confidence}')
+        return []
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(pretrained=True)
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, 6)
+
+    # def forward(self, x):
+    # x = F.relu(self.conv1(x))
+    # x = F.max_pool2d(x, 2, 2)
+    # x = self.conv1_bn(x)
+    # x = F.relu(self.conv2(x))
+    # x = F.max_pool2d(x, 2, 2)
+    # x = x.view(-1, 2048)
+    # x = F.relu(self.fc1(x))
+    # x = self.dropout(x)
+    # x = self.fc2(x)
+    # x = x.view(-1, 1, 512)
+    # x = self.bn(x)
+    # x = x.view(-1, 512)
+    # x = self.fc3(x)
+    # x = self.fc4(x)
+
+    # return x
+
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
+
+
+class Model:
+
+    def __init__(self, trained_weights: str, device: str):
+        self.net = Net()
+        self.weights = trained_weights
+        self.device = torch.device('cuda:0' if device == 'cuda' else 'cpu')
+        self._initialize()
+
+    def _initialize(self):
+        # Load weights
+        try:
+            # Force loading on CPU if there is no GPU
+            if not torch.cuda.is_available():
+                self.net.load_state_dict(torch.load(self.weights, map_location=lambda storage, loc: storage)["state_dict"])
+            else:
+                self.net.load_state_dict(torch.load(self.weights)["state_dict"])
+
+        except IOError:
+            print("Error Loading Weights")
+            return None
+        self.net.eval()
+
+        # Move to specified device
+        self.net.to(self.device)
+
+    def infer(self, path):
+        img = Image.open(path).convert('RGB')
+
+        preprocess = transforms.Compose([
+            transforms.Resize((300, 300)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+        image_tensor = preprocess(img)
+
+        # create a mini-batch as expected by the model
+        input_batch = to_device(image_tensor.unsqueeze(0), self.device)
+
+        with torch.no_grad():
+            output = self.net(input_batch)
+
+        confidence, index = torch.max(output, dim=1)
+
+        """label = "{}: {:.2f}%".format(CLASS_MAPPING[idx], confidence * 100)
+        cv2.rectangle(img, (startX, startY), (endX, endY), COLORS[idx], 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(img, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)"""
+
+        return index[0].item(), confidence[0].item()
+
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+def render_prediction(index):
+    img_class_map = None
+    with open('dashboard/index_to_name.json') as f:
+        img_class_map = json.load(f)
+    stridx = str(index)
+    classname = 'Unknown'
+    if img_class_map is not None:
+        if stridx in img_class_map is not None:
+            classname = img_class_map[stridx]
+
+    return classname
