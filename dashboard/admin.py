dd57c8f9a1957cecad6359c37f4f7331070e2900
charlesdupont <cd.github@4alltime.net>
2023-01-31 13:26:34 +0100

    Modification de l apage dashboard + Ajout lien base de donnée et agrégation des donnés

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dechet
 
 # Register your models here.
+
+
+admin.site.register(Dechet)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class Dechet(models.Model):
+    ANNEE = models.IntegerField()
+    C_REGION = models.IntegerField()
+    L_REGION = models.CharField(max_length=255)
+    C_DEPT = models.CharField(max_length=255)
+    N_DEPT = models.CharField(max_length=255)
+    C_TYP_REG_DECHET = models.CharField(max_length=255)
+    L_TYP_REG_DECHET = models.CharField(max_length=255)
+    TONNAGE_T = models.FloatField()
+    VA_POPANNEE = models.IntegerField()
+    RATION_KG_HAB = models.FloatField()
