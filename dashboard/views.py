73db7e7c83ef819e4fe4c442a6f6a882ebf068d0
charlesdupont <cd.github@4alltime.net>
2023-02-02 18:02:48 +0100

    Dashboard Fonctionnel sans latence

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,6 +19,7 @@ import torch.nn as nn
 import json
 import os
 import certifi
+import time
 
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 os.environ["SSL_CERT_FILE"] = certifi.where()
@@ -39,9 +40,12 @@ def dashboard(request):
                 check_for_duplicate(anonyme_id)
                 detect_type_of_waste(anonyme_id)
     elif request.method == 'GET':
+        start_time = time.time()
         tonnage_total_france, tonnage_total_annee_verre, tonnage_total_annee_plastique, tonnage_total_annee_menager, tonnage_total_region_annee_type, diff_tonnage_france_total, tonnage_total_verre, tonnage_total_plastique, tonnage_total_menager = recover_data()
-
-    return render(request, 'dashboard.html', context={'form': forms.UploadPictureToAnalyze, 'tonnage_total_france': tonnage_total_france, 'diff_tonnage_total_france' : int(diff_tonnage_france_total), 'tonnage_total_annee_verre' : tonnage_total_annee_verre, 'tonnage_total_annee_menager' : tonnage_total_annee_menager, 'tonnage_total_annee_plastique' : tonnage_total_annee_plastique, 'tonnage_total_verre' : tonnage_total_verre, 'tonnage_total_plastique': tonnage_total_plastique, 'tonnage_total_menager': tonnage_total_menager})
+        end_time = time.time()
+        duration = end_time - start_time
+        print("Duration: ", duration)
+    return render(request, 'dashboard.html', context={'form': forms.UploadPictureToAnalyze, 'tonnage_total_france': round(tonnage_total_france/1000, 3), 'diff_tonnage_total_france' : round(diff_tonnage_france_total, 3), 'tonnage_total_annee_verre' : round(tonnage_total_annee_verre/1000, 3), 'tonnage_total_annee_menager' : round(tonnage_total_annee_menager/1000, 3), 'tonnage_total_annee_plastique' : round(tonnage_total_annee_plastique/1000, 3), 'tonnage_total_verre' : tonnage_total_verre, 'tonnage_total_plastique': tonnage_total_plastique, 'tonnage_total_menager': tonnage_total_menager})
 
 
 def mse(image_a, image_b):
@@ -134,28 +138,29 @@ def recover_data():
     tonnage_total_france = tonnage_total_france.get(ANNEE=2019).get('tonnage_total')
     diff_tonnage_france_total = (tonnage_total_france - tonnage_total_france_annne_inf) / tonnage_total_france_annne_inf * 100
 
-    tonnage_total_annee_verre = tonnage_total_annee_type\
+    tonnage_total_annee_verre = int(tonnage_total_annee_type\
         .filter(ANNEE=2019, C_TYP_REG_DECHET='08C')\
-        .first().get('tonnage_total')
+        .aggregate(Sum('tonnage_total')).get('tonnage_total__sum'))
 
-    tonnage_total_annee_plastique = tonnage_total_annee_type\
+    tonnage_total_annee_plastique = int(tonnage_total_annee_type\
         .filter(ANNEE=2019, C_TYP_REG_DECHET='08B')\
-        .first().get('tonnage_total')
+        .aggregate(Sum('tonnage_total')).get('tonnage_total__sum'))
 
-    tonnage_total_annee_menager = tonnage_total_annee_type\
-        .filter(ANNEE=2019, C_TYP_REG_DECHET='08A')\
-        .first().get('tonnage_total')
+    tonnage_total_annee_menager= int(tonnage_total_annee_type\
+    .filter(ANNEE=2019, C_TYP_REG_DECHET__in=['08A', '08D'])\
+    .aggregate(Sum('tonnage_total'))\
+    .get('tonnage_total__sum'))
 
-    tonnage_total_verre = [item.get('tonnage_total') for item in
+    tonnage_total_verre = [int(item.get('tonnage_total')) for item in
                            sorted(tonnage_total_annee_type.filter(C_TYP_REG_DECHET='08C'), key=lambda x: x.get('ANNEE'))]
 
-    tonnage_total_plastique = [item.get('tonnage_total') for item in
+    tonnage_total_plastique = [int(item.get('tonnage_total')) for item in
                                sorted(tonnage_total_annee_type.filter(C_TYP_REG_DECHET='08B'), key=lambda x: x.get('ANNEE'))]
 
-    tonnage_total_menager = [item.get('tonnage_total') for item in
-                             sorted(tonnage_total_annee_type.filter(C_TYP_REG_DECHET='08A'), key=lambda x: x.get('ANNEE'))]
+    tonnage_total_menager = [int(item.get('tonnage_total')) for item in
+                             sorted(tonnage_total_annee_type.filter(C_TYP_REG_DECHET__in =['08A', '08D']), key=lambda x: x.get('ANNEE'))]
 
-    return int(tonnage_total_france), int(tonnage_total_annee_verre),int(tonnage_total_annee_plastique),int(tonnage_total_annee_menager), tonnage_total_region_annee_type,int(diff_tonnage_france_total), tonnage_total_verre,tonnage_total_plastique, tonnage_total_menager
+    return int(tonnage_total_france), tonnage_total_annee_verre,tonnage_total_annee_plastique,tonnage_total_annee_menager, tonnage_total_region_annee_type,diff_tonnage_france_total, tonnage_total_verre,tonnage_total_plastique, tonnage_total_menager
 
 
 
